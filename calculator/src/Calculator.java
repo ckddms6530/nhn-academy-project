6dedd6ee6fbccd6e25063ece56a418631ef293ab
{ckddms6530} <{ckddms6530@gmail.com}>
2023-09-04 19:22:05 +0900

    계산기 기능 구현

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -1,12 +1,19 @@
 public class Calculator {
 
+    static void cal (int num1 , int num2) {
+        Div div = new Div();
+        System.out.println(Sum.sum(num1, num2));
+        System.out.println(Sub.sub(num1, num2));
+        System.out.println(div.div(num1, num2));
+        System.out.println(Multi.multi(num1, num2));
+    }
 
 
 
     public static void main(String[] args) throws Exception {
 
 
-        
+        cal(5, 5);
 
         
     }
